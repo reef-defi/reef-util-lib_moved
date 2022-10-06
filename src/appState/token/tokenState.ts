0a805df601cfb965910261c231e634edb7d55809
Matjaz Hirsman <matjazonline@gmail.com>
2022-10-06 15:41:27 +0200

    test circular deps

--- src/appState/token/tokenState.ts
+++ src/appState/token/tokenState.ts
@@ -1,12 +1,12 @@
 import {combineLatest, map, Observable, shareReplay,} from 'rxjs';
-import {pools$, selectedSignerPools$} from "./pools";
+import {selectedSignerPools$} from "./pools";
 import {TokenWithAmount} from "../../token/token";
 import {toTokensWithPrice} from "../util/util";
 import {reefPrice$} from "./reefPrice";
 import {selectedSignerTokenBalances$} from "./selectedSignerTokenBalances";
 
 // TODO pools and tokens emit events at same time - check how to make 1 event from it
-export const selectedSignerTokenPrices$: Observable<TokenWithAmount[]> = combineLatest([
+export const signerTokenPrices$: Observable<TokenWithAmount[]> = combineLatest([
     selectedSignerTokenBalances$,
     reefPrice$,
     selectedSignerPools$,
