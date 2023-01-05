1d23f66c55f120c86e520b529d27acce73be4e1a
matjazonline <matjazonline@gmail.com>
2023-01-05 19:17:44 +0100

    export gql queries

--- src/graphql/index.ts
+++ src/graphql/index.ts
@@ -1,4 +1,7 @@
 export {apolloClientInstance$, zenToRx} from './apollo';
+export {SIGNER_TOKENS_GQL, CONTRACT_DATA_GQL} from './signerTokens.gql';
+export {SIGNER_NFTS_GQL} from './signerNfts.gql';
+export {TRANSFER_HISTORY_GQL} from './transferHistory.gql';
 // export {getGQLUrls} from "./gqlUtil";
 // export * from './signerNfts.gql'
 // export * from './availablePools.gql'
