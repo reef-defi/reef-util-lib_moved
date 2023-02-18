5e292db8389a13c086a47c39f0a25038e29b72e1
matjazonline <matjazonline@gmail.com>
2023-02-18 19:12:40 +0100

    filter account tokens to balance gt 0

--- src/graphql/signerTokens.gql.ts
+++ src/graphql/signerTokens.gql.ts
@@ -3,7 +3,7 @@ import {gql} from "@apollo/client";
 export const SIGNER_TOKENS_GQL = gql`
   subscription tokens_query($accountId: String!) {
    tokenHolders(
-        where: {AND: {nftId_isNull: true, token: {id_isNull: false}, signer: {id_eq: $accountId}}},
+        where: {AND: {nftId_isNull: true, token: {id_isNull: false}, signer: {id_eq: $accountId}, balance_gt: "0"}},
         orderBy: balance_DESC,
         limit: 320
     ) {
