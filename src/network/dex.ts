1efd5aff53e9aa3c35c33f939d58d24e2728d016
boo-0x <boo_0x@tutanota.com>
2023-04-06 10:10:36 +0200

    Update Network and DEX config

--- src/network/dex.ts
+++ src/network/dex.ts
@@ -1,23 +1,27 @@
-import {Network, NetworkName} from "./network";
+import {Network} from "./network";
 
 export const REEFSWAP_CONFIG: { [networkName: string]: DexProtocolv2 } = {
     mainnet: {
-        routerAddress: '0x641e34931C03751BFED14C4087bA395303bEd1A5',
         factoryAddress: '0x380a9033500154872813F6E1120a81ed6c0760a8',
+        routerAddress: '0x641e34931C03751BFED14C4087bA395303bEd1A5',
+        graphqlDexsUrl: "https://squid.subsquid.io/reef-swap/graphql",
     },
     testnet: {
-        factoryAddress: '0xcA36bA38f2776184242d3652b17bA4A77842707e',
-        routerAddress: '0x0A2906130B1EcBffbE1Edb63D5417002956dFd41'
+        factoryAddress: '0x06D7a7334B9329D0750FFd0a636D6C3dFA77E580',
+        routerAddress: '0xa29DFc7329ac30445Ba963E313fD26E171722057',
+        graphqlDexsUrl: "https://squid.subsquid.io/reef-swap-testnet/graphql",
     },
     /*localhost: {
         factoryAddress: '0xD3ba2aA7dfD7d6657D5947f3870A636c7351EfE4',
         routerAddress: '0x818Be9d50d84CF31dB5cefc7e50e60Ceb73c1eb5',
+        graphqlDexsUrl: "http://localhost:4351/graphql",
     }*/
 };
 
 export interface DexProtocolv2 {
     routerAddress: string;
     factoryAddress: string;
+    graphqlDexsUrl: string;
 }
 
 export const getReefswapNetworkConfig = (network: Network): DexProtocolv2 => {
